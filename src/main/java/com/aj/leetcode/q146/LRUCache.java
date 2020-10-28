f588bd2679e8521f5df346ad6d888f2f09153539
cfrs2005 <cfrs2005@gmail.com>
2020-10-29 00:29:22 +0800

    add q146 question

---
+++ src/main/java/com/aj/leetcode/q146/LRUCache.java
@@ -0,0 +1,31 @@
+package com.aj.leetcode.q146;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/10/28
+ */
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
