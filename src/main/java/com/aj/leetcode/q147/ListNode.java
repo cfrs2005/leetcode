7ca7d43b43733f4c68f0654c1a4bc44d6251532c
cfrs2005 <cfrs2005@gmail.com>
2020-11-20 21:19:20 +0800

    add q147

---
+++ src/main/java/com/aj/leetcode/q147/ListNode.java
@@ -0,0 +1,14 @@
+package com.aj.leetcode.q147;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/11/20
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/aj/leetcode/q147/Solution.java
@@ -0,0 +1,32 @@
+package com.aj.leetcode.q147;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/11/20
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head;
+        ListNode curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val < curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+}
