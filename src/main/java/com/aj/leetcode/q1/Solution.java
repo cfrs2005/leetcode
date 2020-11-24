a59340becded045d42fbdaaf262545614d460e20
cfrs2005 <cfrs2005@gmail.com>
2020-11-24 22:51:28 +0800

    add q1

---
+++ src/main/java/com/aj/leetcode/q1/Solution.java
@@ -0,0 +1,22 @@
+package com.aj.leetcode.q1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/11/24
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result)) {
+                return new int[]{map.get(i), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
