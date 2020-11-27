deb6b655081537bdcf49c86f05357e40ded1d5d8
cfrs2005 <cfrs2005@gmail.com>
2020-11-27 23:05:11 +0800

    add q 454

---
+++ src/main/java/com/aj/leetcode/q454/Solution.java
@@ -0,0 +1,30 @@
+package com.aj.leetcode.q454;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/11/27
+ */
+public class Solution {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+
+        Map<Integer, Integer> countAB = new HashMap<Integer, Integer>();
+        for (int u : A) {
+            for (int v : B) {
+                countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : C) {
+            for (int v : D) {
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+
+}
