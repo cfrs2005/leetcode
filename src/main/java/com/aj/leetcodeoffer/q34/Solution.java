6e73ac03fd9fe143ac0d71ad5f91f0224a47f8f5
cfrs2005 <cfrs2005@gmail.com>
2020-12-01 22:17:26 +0800

    add q 34

---
+++ src/main/java/com/aj/leetcodeoffer/q34/Solution.java
@@ -0,0 +1,32 @@
+package com.aj.leetcodeoffer.q34;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/12/1
+ */
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx <= nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
