96b084a056675c23bc842fd752a77d179553e067
cfrs2005 <cfrs2005@gmail.com>
2020-12-02 22:32:18 +0800

    add offer q14

---
+++ src/main/java/com/aj/leetcodeoffer/q14/Solution.java
@@ -0,0 +1,23 @@
+package com.aj.leetcodeoffer.q14;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/12/2
+ */
+public class Solution {
+    public int cuttingRope(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int a = n / 3;
+        int b = n % 3;
+        if (b == 0) {
+            return (int) Math.pow(3, a);
+        }
+        if (b == 1) {
+            return (int) Math.pow(3, a - 1) * 4;
+        }
+        return (int) Math.pow(3, a) * 2;
+    }
+
+}
