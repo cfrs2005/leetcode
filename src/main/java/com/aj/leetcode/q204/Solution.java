3169d7d8aa205b38f1d17e72047848d4a5801e97
cfrs2005 <cfrs2005@gmail.com>
2020-12-03 22:31:31 +0800

    add q204

---
+++ src/main/java/com/aj/leetcode/q204/Solution.java
@@ -0,0 +1,29 @@
+package com.aj.leetcode.q204;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/12/3
+ */
+public class Solution {
+    /**
+     * @param n
+     * @return
+     */
+    public int countPrimes(int n) {
+        int ans = 0;
+        for (int i = 2; i < n; ++i) {
+            ans += isPrime(i) ? 1 : 0;
+        }
+        return ans;
+    }
+
+    public boolean isPrime(int x) {
+        for (int i = 2; i * i <= x; ++i) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
