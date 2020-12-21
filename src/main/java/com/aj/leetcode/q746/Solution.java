c53534ca6f2fc2f47db67e0afdee916ceb62cc4d
cfrs2005 <cfrs2005@gmail.com>
2020-12-21 21:43:22 +0800

    add q746

---
+++ src/main/java/com/aj/leetcode/q746/Solution.java
@@ -0,0 +1,16 @@
+package com.aj.leetcode.q746;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/12/21
+ */
+public class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.length];
+    }
+}
