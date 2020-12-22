60e9cfa166ab5e89d0f47dabf7b21035ef793813
cfrs2005 <cfrs2005@gmail.com>
2020-12-22 21:54:59 +0800

    add q103

---
+++ src/main/java/com/aj/leetcode/q103/TreeNode.java
@@ -0,0 +1,15 @@
+package com.aj.leetcode.q103;
+
+/**
+ * @author zhangqingyue
+ * @date 2020/12/22
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
